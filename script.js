c1c78653598a701037b3e21310ffa425ee5481d8
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-07-03 10:51:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+let mostraHTML = document.getElementById("mostraHTML");
+
+let nome1;
+let nome2;
+let nome3;
+let nome4;
+let nome5;
+let nome6;
+let nome7;
+let nome8;
+let nome9;
+let nome10;
+let nome11;
+let nome12;
+    let idade1;
+    let idade2;
+    let idade3;
+    let idade4;
+    let idade5;
+    let idade6;
+    let idade7;
+    let idade8;
+    let idade9;
+    let idade10;
+    let idade11;
+    let idade12;
+        let time1;
+        let time2;
+        let time3;
+
+function cadastro(){
+
+    time1 =(window.prompt("qual o nome do seu time"));
+    jhonHTML.innerHTML +=`<p> time-${time1}`;
+    
+    
+  
+      nome1 =(window.prompt("qual o seu nome"));
+    
+    idade1 =(window.prompt("qual sua idade"));
+     jhonHTML.innerHTML +=`<p> ${nome1}, idade:${idade1}`;
+
+
+      nome2=(window.prompt("qual o seu nome"));
+    
+    idade2 =(window.prompt("qual sua idade"));
+     jhonHTML.innerHTML +=`<p> ${nome2}, idade:${idade2}`;
+
+
+      nome3 =(window.prompt("qual o seu nome"));
+    
+    idade3  =(window.prompt("qual sua idade"));
+    jhonHTML.innerHTML +=`<p> ${nome3}, idade:${idade3}`;
+
+
+
+
+
+
+
+
+
+      time2 =(window.prompt("qual o nome do seu time"));
+    mostraHTML.innerHTML +=`<p> time-${time2}`;
+    
+    
+  
+      nome4 =(window.prompt("qual o seu nome"));
+    
+    idade4 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome4}, idade:${idade4}`;
+
+
+      nome5=(window.prompt("qual o seu nome"));
+    
+    idade5 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome5}, idade:${idade5}`;
+
+
+      nome6 =(window.prompt("qual o seu nome"));
+    
+    idade6  =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome6}, idade:${idade6}`;
+
+
+
+
+
+
+
+    
+     time3 =(window.prompt("qual o nome do seu time"));
+    mostraHTML.innerHTML +=`<p> time-${time3}`;
+    
+    
+  
+      nome7 =(window.prompt("qual o seu nome"));
+    
+    idade7 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome7}, idade:${idade7}`;
+
+
+      nome8 =(window.prompt("qual o seu nome"));
+    
+    idade8 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome8}, idade:${idade8}`;
+
+
+      nome9=(window.prompt("qual o seu nome"));
+    
+    idade9  =(window.prompt("qual sua idade"));
+      mostraHTML.innerHTML +=`<p> ${nome9}, idade:${idade9}`;
+
+
+
+
+
+          
+     time4 =(window.prompt("qual o nome do seu time"));
+    mostraHTML.innerHTML +=`<p> time-${time4}`;
+    
+    
+  
+      nome10 =(window.prompt("qual o seu nome"));
+    
+    idade10 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome10}, idade:${idade10}`;
+
+
+      nome11 =(window.prompt("qual o seu nome"));
+    
+    idade11 =(window.prompt("qual sua idade"));
+    mostraHTML.innerHTML +=`<p> ${nome11}, idade:${idade11}`;
+
+
+      nome12=(window.prompt("qual o seu nome"));
+    
+    idade12  =(window.prompt("qual sua idade"));
+      mostraHTML.innerHTML +=`<p> ${nome12}, idade:${idade12}`;
+
+
+      if(times = 4){
+        alert("PAREEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!JA DEU 4 TIMES. SE CONTINUAR VOU COMER VC COMUMA PICA DE PLASTICO DE 1 METRO")
+      }
+    
+
+}
