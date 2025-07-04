8fd33a72d2045f0ba76b638551d6e39f70e523c8
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-07-04 11:39:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,31 +29,41 @@ let nome12;
         let time2;
         let time3;
         let time4;
+         let media1 = 0;
+         let media2;
+         let media3;
+         let media4;
+
 
 function cadastro(){
 
     time1 =(window.prompt("qual o nome do seu time"));
-    frontHTML.innerHTML +=`<p> time-${time1}</p>`;
+    mostraHTML.innerHTML +=`<p> time-${time1}</p>`;
     
     
   
       nome1 =(window.prompt("qual o seu nome"));
     
     idade1 =(window.prompt("qual sua idade"));
-     frontHTML.innerHTML +=`<p> ${nome1}, idade:${idade1}</p>`;
+     mostraHTML.innerHTML +=`<p> ${nome1}, idade:${idade1}</p>`;
 
 
       nome2=(window.prompt("qual o seu nome"));
     
     idade2 =(window.prompt("qual sua idade"));
-     frontHTML.innerHTML +=`<p> ${nome2}, idade:${idade2}</p>`;
+     mostraHTML.innerHTML +=`<p> ${nome2}, idade:${idade2}</p>`;
 
 
       nome3 =(window.prompt("qual o seu nome"));
     
     idade3  =(window.prompt("qual sua idade"));
-    frontHTML.innerHTML +=`<p> ${nome3}, idade:${idade3}</p>`;
+    mostraHTML.innerHTML +=`<p> ${nome3}, idade:${idade3}</p>`;
 
+    media1 = ((idade1 + idade2 + idade3) / 3);
+
+
+    mostraHTML.innerHTML +=`<p> media:${media1}</p>`;
+    
 
 
 
@@ -63,26 +73,26 @@ function cadastro(){
 
 
       time2 =(window.prompt("qual o nome do seu time"));
-    mostraHTML.innerHTML +=`<p> time-${time2}</p>`;
+    jhonHTML.innerHTML +=`<p> time-${time2}</p>`;
     
     
   
       nome4 =(window.prompt("qual o seu nome"));
     
     idade4 =(window.prompt("qual sua idade"));
-    mostraHTML.innerHTML +=`<p> ${nome4}, idade:${idade4}</p>`;
+    jhonHTML.innerHTML +=`<p> ${nome4}, idade:${idade4}</p>`;
 
 
       nome5=(window.prompt("qual o seu nome"));
     
     idade5 =(window.prompt("qual sua idade"));
-    mostraHTML.innerHTML +=`<p> ${nome5}, idade:${idade5}</p>`;
+    jhonHTML.innerHTML +=`<p> ${nome5}, idade:${idade5}</p>`;
 
 
       nome6 =(window.prompt("qual o seu nome"));
     
     idade6  =(window.prompt("qual sua idade"));
-    mostraHTML.innerHTML +=`<p> ${nome6}, idade:${idade6}</p>`;
+    jhonHTML.innerHTML +=`<p> ${nome6}, idade:${idade6}</p>`;
 
 
 
@@ -92,26 +102,26 @@ function cadastro(){
 
     
      time3 =(window.prompt("qual o nome do seu time"));
-    leHTML.innerHTML +=`<p> time-${time3}</p>`;
+    frontHTML.innerHTML +=`<p> time-${time3}</p>`;
     
     
   
       nome7 =(window.prompt("qual o seu nome"));
     
     idade7 =(window.prompt("qual sua idade"));
-    leHTML.innerHTML +=`<p> ${nome7}, idade:${idade7}</p>`;
+    frontHTML.innerHTML +=`<p> ${nome7}, idade:${idade7}</p>`;
 
 
       nome8 =(window.prompt("qual o seu nome"));
     
     idade8 =(window.prompt("qual sua idade"));
-    leHTML.innerHTML +=`<p> ${nome8}, idade:${idade8}</p>`;
+    frontHTML.innerHTML +=`<p> ${nome8}, idade:${idade8}</p>`;
 
 
       nome9=(window.prompt("qual o seu nome"));
     
     idade9  =(window.prompt("qual sua idade"));
-      leHTML.innerHTML +=`<p> ${nome9}, idade:${idade9}</p>`;
+      frontHTML.innerHTML +=`<p> ${nome9}, idade:${idade9}</p>`;
 
 
 
@@ -119,38 +129,36 @@ function cadastro(){
 
           
      time4 =(window.prompt("qual o nome do seu time"));
-    jhonHTML.innerHTML +=`<p> time-${time4}</p>`;
+    leHTML.innerHTML +=`<p> time-${time4}</p>`;
     
     
   
       nome10 =(window.prompt("qual o seu nome"));
     
     idade10 =(window.prompt("qual sua idade"));
-    jhonHTML.innerHTML +=`<p> ${nome10}, idade:${idade10}</p>`;
+    leHTML.innerHTML +=`<p> ${nome10}, idade:${idade10}</p>`;
 
 
       nome11 =(window.prompt("qual o seu nome"));
     
     idade11 =(window.prompt("qual sua idade"));
-    jhonHTML.innerHTML +=`<p> ${nome11}, idade:${idade11}</p>`;
+    leHTML.innerHTML +=`<p> ${nome11}, idade:${idade11}</p>`;
 
 
       nome12=(window.prompt("qual o seu nome"));
     
     idade12  =(window.prompt("qual sua idade"));
-      jhonHTML.innerHTML +=`<p> ${nome12}, idade:${idade12}</p>`;
+      leHTML.innerHTML +=`<p> ${nome12}, idade:${idade12}</p>`;
 
 
-      if(time1){ 
-        mostraHTML.innerHTML += `<b> <p> ${time1} </p></b>`;  
-       } else {
-        mostraHTML.innerHTML += `<p> ${time1} </p>`; 
-       }
+    
 
 
       if(times = 4){
         alert("PAREEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!JA DEU 4 TIMES. SE CONTINUAR VOU COMER VC COMUMA PICA DE PLASTICO DE 1 METRO")
-      }else{}
+      }else{
+       
+      }
 
       
     }
