20fda809f4a2a39632fbafd28a4242a79ef18916
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-07-06 19:09:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,164 +1,155 @@
 let mostraHTML = document.getElementById("mostraHTML");
 let jhonHTML = document.getElementById("jhonHTML");
+let frontHTML = document.getElementById("frontHTML");
+let leHTML = document.getElementById("leHTML");
 
-let nome1;
-let nome2;
-let nome3;
-let nome4;
-let nome5;
-let nome6;
-let nome7;
-let nome8;
-let nome9;
-let nome10;
-let nome11;
-let nome12;
-    let idade1;
-    let idade2;
-    let idade3;
-    let idade4;
-    let idade5;
-    let idade6;
-    let idade7;
-    let idade8;
-    let idade9;
-    let idade10;
-    let idade11;
-    let idade12;
-        let time1;
-        let time2;
-        let time3;
-        let time4;
-         let media1 = 0;
-         let media2;
-         let media3;
-         let media4;
+function cadastro() {
+  // 1
+  let time1 = window.prompt("Qual o nome do Time 1?");
+  mostraHTML.innerHTML += `<h3>Time: ${time1}</h3>`;
+
+  let nome1 = window.prompt("Qual seu nome?");
+  let idade1 = Number(window.prompt("Jogador 1 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome1}, Idade: ${idade1}</p>`;
+
+  let nome2 = window.prompt("Qual seu nome?");
+  let idade2 = Number(window.prompt("Jogador 2 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome2}, Idade: ${idade2}</p>`;
+
+  let nome3 = window.prompt("Qual seu nome?");
+  let idade3 = Number(window.prompt("Jogador 3 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome3}, Idade: ${idade3}</p>`;
+
+  let media1 = (idade1 + idade2 + idade3) / 3;
+  mostraHTML.innerHTML += `<p>Média de idade: ${media1.toFixed(2)}</p><hr>`;
 
 
-function cadastro(){
 
-    time1 =(window.prompt("qual o nome do seu time"));
-    mostraHTML.innerHTML +=`<p> time-${time1}</p>`;
-    
-    
+
+  if (media1 > 15) {
+    alert("vcs não poderão jogar o campeonato");
+
   
-      nome1 =(window.prompt("qual o seu nome"));
-    
-    idade1 =(window.prompt("qual sua idade"));
-     mostraHTML.innerHTML +=`<p> ${nome1}, idade:${idade1}</p>`;
+  mostrarHTML.innerHTML = `vocês estão eliminado do campeonato. motivo:sua media de idade eceder o limite colocado`;
 
-
-      nome2=(window.prompt("qual o seu nome"));
-    
-    idade2 =(window.prompt("qual sua idade"));
-     mostraHTML.innerHTML +=`<p> ${nome2}, idade:${idade2}</p>`;
-
-
-      nome3 =(window.prompt("qual o seu nome"));
-    
-    idade3  =(window.prompt("qual sua idade"));
-    mostraHTML.innerHTML +=`<p> ${nome3}, idade:${idade3}</p>`;
-
-    media1 = ((idade1 + idade2 + idade3) / 3);
-
-
-    mostraHTML.innerHTML +=`<p> media:${media1}</p>`;
-    
-
-
-
-
-
-
-
-
-      time2 =(window.prompt("qual o nome do seu time"));
-    jhonHTML.innerHTML +=`<p> time-${time2}</p>`;
-    
-    
-  
-      nome4 =(window.prompt("qual o seu nome"));
-    
-    idade4 =(window.prompt("qual sua idade"));
-    jhonHTML.innerHTML +=`<p> ${nome4}, idade:${idade4}</p>`;
-
-
-      nome5=(window.prompt("qual o seu nome"));
-    
-    idade5 =(window.prompt("qual sua idade"));
-    jhonHTML.innerHTML +=`<p> ${nome5}, idade:${idade5}</p>`;
-
-
-      nome6 =(window.prompt("qual o seu nome"));
-    
-    idade6  =(window.prompt("qual sua idade"));
-    jhonHTML.innerHTML +=`<p> ${nome6}, idade:${idade6}</p>`;
-
-
-
-
-
-
-
-    
-     time3 =(window.prompt("qual o nome do seu time"));
-    frontHTML.innerHTML +=`<p> time-${time3}</p>`;
-    
-    
-  
-      nome7 =(window.prompt("qual o seu nome"));
-    
-    idade7 =(window.prompt("qual sua idade"));
-    frontHTML.innerHTML +=`<p> ${nome7}, idade:${idade7}</p>`;
-
-
-      nome8 =(window.prompt("qual o seu nome"));
-    
-    idade8 =(window.prompt("qual sua idade"));
-    frontHTML.innerHTML +=`<p> ${nome8}, idade:${idade8}</p>`;
-
-
-      nome9=(window.prompt("qual o seu nome"));
-    
-    idade9  =(window.prompt("qual sua idade"));
-      frontHTML.innerHTML +=`<p> ${nome9}, idade:${idade9}</p>`;
-
-
-
-
-
-          
-     time4 =(window.prompt("qual o nome do seu time"));
-    leHTML.innerHTML +=`<p> time-${time4}</p>`;
-    
-    
-  
-      nome10 =(window.prompt("qual o seu nome"));
-    
-    idade10 =(window.prompt("qual sua idade"));
-    leHTML.innerHTML +=`<p> ${nome10}, idade:${idade10}</p>`;
-
-
-      nome11 =(window.prompt("qual o seu nome"));
-    
-    idade11 =(window.prompt("qual sua idade"));
-    leHTML.innerHTML +=`<p> ${nome11}, idade:${idade11}</p>`;
-
-
-      nome12=(window.prompt("qual o seu nome"));
-    
-    idade12  =(window.prompt("qual sua idade"));
-      leHTML.innerHTML +=`<p> ${nome12}, idade:${idade12}</p>`;
-
-
-    
-
-
-      if(times = 4){
-        alert("PAREEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!JA DEU 4 TIMES. SE CONTINUAR VOU COMER VC COMUMA PICA DE PLASTICO DE 1 METRO")
-      }else{
-       
-      }
-
-      
+    while (media >= 15 || media === 0) {
+    mostraHTML.innerHTML = "";
     }
+
+      let time1 = window.prompt("Qual o nome do Time 1?");
+  mostraHTML.innerHTML += `<h3>Time: ${time1}</h3>`;
+
+  let nome1 = window.prompt("Qual seu nome?");
+  let idade1 = Number(window.prompt("Jogador 1 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome1}, Idade: ${idade1}</p>`;
+
+  let nome2 = window.prompt("Qual seu nome?");
+  let idade2 = Number(window.prompt("Jogador 2 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome2}, Idade: ${idade2}</p>`;
+
+  let nome3 = window.prompt("Qual seu nome?");
+  let idade3 = Number(window.prompt("Jogador 3 - Qual sua idade?"));
+
+  mostraHTML.innerHTML += `<p>${nome3}, Idade: ${idade3}</p>`;
+
+  let media1 = (idade1 + idade2 + idade3) / 3;
+  mostraHTML.innerHTML += `<p>Média de idade: ${media1.toFixed(2)}</p><hr>`;
+
+
+  } else {
+    alert("o campeonato vai começar em 00/00/0000");
+  }
+
+  // 2
+  let time2 = window.prompt("Qual o nome do Time 2?");
+  jhonHTML.innerHTML += `<h3>Time: ${time2}</h3>`;
+
+  let nome4 = window.prompt("Qual seu nome?");
+  let idade4 = Number(window.prompt("Jogador 1 - Qual sua idade?"));
+
+  jhonHTML.innerHTML += `<p>${nome4}, Idade: ${idade4}</p>`;
+
+  let nome5 = window.prompt("Qual seu nome?");
+  let idade5 = Number(window.prompt("Jogador 2 - Qual sua idade?"));
+
+  jhonHTML.innerHTML += `<p>${nome5}, Idade: ${idade5}</p>`;
+
+  let nome6 = window.prompt("Qual seu nome?");
+  let idade6 = Number(window.prompt("Jogador 3 - Qual sua idade?"));
+
+  jhonHTML.innerHTML += `<p>${nome6}, Idade: ${idade6}</p>`;
+
+  let media2 = (idade4 + idade5 + idade6) / 3;
+  jhonHTML.innerHTML += `<p>Média de idade: ${media2.toFixed(2)}</p><hr>`;
+
+  if (media2 > 15) {
+    alert("vcs não poderão jogar o campeonato");
+  } else {
+    alert("o campeonato vai começar em 00/00/0000");
+  }
+
+  // 3
+  let time3 = window.prompt("Qual o nome do Time 3?");
+  frontHTML.innerHTML += `<h3>Time: ${time3}</h3>`;
+
+  let nome7 = window.prompt("Qual seu nome?");
+  let idade7 = Number(window.prompt("Jogador 1 - Qual sua idade?"));
+
+  frontHTML.innerHTML += `<p>${nome7}, Idade: ${idade7}</p>`;
+
+  let nome8 = window.prompt("Qual seu nome?");
+  let idade8 = Number(window.prompt("Jogador 2 - Qual sua idade?"));
+
+  frontHTML.innerHTML += `<p>${nome8}, Idade: ${idade8}</p>`;
+
+  let nome9 = window.prompt("Qual seu nome?");
+  let idade9 = Number(window.prompt("Jogador 3 - Qual sua idade?"));
+
+  frontHTML.innerHTML += `<p>${nome9}, Idade: ${idade9}</p>`;
+
+  let media3 = (idade7 + idade8 + idade9) / 3;
+  frontHTML.innerHTML += `<p>Média de idade: ${media3.toFixed(2)}</p><hr>`;
+
+  if (media3 > 15) {
+    alert("vcs não poderão jogar o campeonato");
+  } else {
+    alert("o campeonato vai começar em 00/00/0000");
+  }
+
+  // 4
+  let time4 = window.prompt("Qual o nome do Time 4?");
+  leHTML.innerHTML += `<h3>Time: ${time4}</h3>`;
+
+  let nome10 = window.prompt("Qual seu nome?");
+  let idade10 = Number(window.prompt("Jogador 1 - Qual sua idade?"));
+
+  leHTML.innerHTML += `<p>${nome10}, Idade: ${idade10}</p>`;
+
+  let nome11 = window.prompt("Qual seu nome?");
+  let idade11 = Number(window.prompt("Jogador 2 - Qual sua idade?"));
+
+  leHTML.innerHTML += `<p>${nome11}, Idade: ${idade11}</p>`;
+
+  let nome12 = window.prompt("Qual seu nome?");
+  let idade12 = Number(window.prompt("Jogador 3 - Qual sua idade?"));
+
+  leHTML.innerHTML += `<p>${nome12}, Idade: ${idade12}</p>`;
+
+  let media4 = (idade10 + idade11 + idade12) / 3;
+  leHTML.innerHTML += `<p>Média de idade: ${media4.toFixed(2)}</p><hr>`;
+
+  if (media4 > 15) {
+    alert("vcs não poderão jogar o campeonato");
+  } else {
+    alert("o campeonato vai começar em 00/00/0000");
+  }
+
+  alert("Cadastro finalizado! 4 times cadastrados com sucesso.");
+
+  alert("desculpe mais é só quatro times");
+}
